891eb3c3ba40079588598c4dd631f0517dc8577f
Bruno Correia <brunogomescorreia@gmail.com>
2018-03-01 19:21:32 +0000

    Update

--- hooks/deploy.php
+++ hooks/deploy.php
@@ -2,10 +2,22 @@
 	/**
 	 * GIT DEPLOYMENT SCRIPT
 	 */
-
+/*
 	// The commands
 	$commands = array(
 		'git pull',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+*/
+
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
 		'git submodule sync',
 		'git submodule update',
 		'git submodule status',
