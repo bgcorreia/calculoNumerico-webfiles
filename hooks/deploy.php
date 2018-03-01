9ac8f5538734b821578c91943adb1c08bbe0eaa8
Bruno Correia <brunogomescorreia@gmail.com>
2018-03-01 16:29:10 -0300

    Update

--- hooks/deploy.php
+++ hooks/deploy.php
@@ -1,17 +1,11 @@
 <?php
 	/**
 	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
 	 */
-/*
-	// The commands
-	$commands = array(
-		'git pull',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
-*/
-
 	// The commands
 	$commands = array(
 		'echo $PWD',
@@ -22,16 +16,14 @@
 		'git submodule update',
 		'git submodule status',
 	);
-
 	// Run the commands for output
+	$output = '';
 	foreach($commands AS $command){
 		// Run it
-//		shell_exec($command);
-				// Run it
 		$tmp = shell_exec($command);
 		// Output
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}
-
-}
+	// Make it pretty for manual user access (and why not?)
+?>
